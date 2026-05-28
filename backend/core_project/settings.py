1f45b979cb14c5e6b257e1a512a6725e8583d108
Amit <amitshantijyala2004@gmail.com>
2026-05-28 17:54:18 +0530

    error correction

--- backend/core_project/settings.py
+++ backend/core_project/settings.py
@@ -26,13 +26,7 @@ SECRET_KEY = os.environ.get("DJANGO_SECRET_KEY", "fallback-for-local-only")
 DEBUG = os.environ.get("DEBUG", "False") == "True"
 ALLOWED_HOSTS = os.environ.get("ALLOWED_HOSTS", "").split(",")
 CORS_ALLOWED_ORIGINS = os.environ.get("CORS_ALLOWED_ORIGINS", "").split(",")
-
-# SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
-
-ALLOWED_HOSTS = []
-
-
+CSRF_TRUSTED_ORIGINS = os.environ.get("CSRF_TRUSTED_ORIGINS", "").split(",")
 # Application definition
 
 INSTALLED_APPS = [
